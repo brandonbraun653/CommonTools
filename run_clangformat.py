5a09897af9d23fa395cbb51fac68e7c72f451d69
Brandon Braun <brandonbraun653@gmail.com>
2019-02-16 10:59:56 -0600

    Update submodules and clangformatter

--- run_clangformat.py
+++ run_clangformat.py
@@ -30,19 +30,15 @@ def run_clang_format():
     parser.add_argument('-f', dest='format_file', default='',
                         help='Gives the path to the format .json file, relative to this script')
 
-
-    
     args = parser.parse_args()
 
-    working_dir = os.path.join(os.path.dirname(__file__))
-    print("This is the working dir: {}".format(working_dir))
+    format_file = os.path.realpath(os.path.join(os.getcwd(), args.format_file))
+    working_dir = os.path.dirname(format_file)
+    print("Working Dir: {}".format(working_dir))
+    print("Format File: {}".format(format_file))
 
-    format_file = os.path.realpath(os.path.join(working_dir, args.format_file))
-    print("This is the file given: {}".format(format_file))
-
-    #cf = ClangFormatter(project_file=format_file, working_dir=working_dir, clang_format_exe=swqa.clang_format_exe)
-    #return cf.execute()
-    return 0
+    cf = ClangFormatter(project_file=format_file, working_dir=working_dir, clang_format_exe=swqa.clang_format_exe)
+    return cf.execute()
 
 
 if __name__ == "__main__":
