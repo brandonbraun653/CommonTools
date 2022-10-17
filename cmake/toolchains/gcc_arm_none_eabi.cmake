b2aafe75a5df467779244a6ed0cbe22c4afe25e4
Brandon Braun <brandonbraun653@protonmail.com>
2022-10-17 12:03:16 -0700

    Update path for arm_none_eabi compiler

--- cmake/toolchains/gcc_arm_none_eabi.cmake
+++ cmake/toolchains/gcc_arm_none_eabi.cmake
@@ -21,23 +21,23 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
 # C Compiler
-set(CMAKE_C_COMPILER "${ARM_NONE_EABI_ROOT}/arm-none-eabi-gcc")
-set(CMAKE_C_COMPILER_AR "${ARM_NONE_EABI_ROOT}/arm-none-eabi-ar")
-set(CMAKE_C_COMPILER_RANLIB "${ARM_NONE_EABI_ROOT}/arm-none-eabi-ranlib")
+set(CMAKE_C_COMPILER "${ARM_NONE_EABI_ROOT}arm-none-eabi-gcc")
+set(CMAKE_C_COMPILER_AR "${ARM_NONE_EABI_ROOT}arm-none-eabi-ar")
+set(CMAKE_C_COMPILER_RANLIB "${ARM_NONE_EABI_ROOT}arm-none-eabi-ranlib")
 
 # C++ Compiler
-set(CMAKE_CXX_COMPILER "${ARM_NONE_EABI_ROOT}/arm-none-eabi-g++")
-set(CMAKE_CXX_COMPILER_AR "${ARM_NONE_EABI_ROOT}/arm-none-eabi-ar")
-set(CMAKE_CXX_COMPILER_RANLIB "${ARM_NONE_EABI_ROOT}/arm-none-eabi-ranlib")
+set(CMAKE_CXX_COMPILER "${ARM_NONE_EABI_ROOT}arm-none-eabi-g++")
+set(CMAKE_CXX_COMPILER_AR "${ARM_NONE_EABI_ROOT}arm-none-eabi-ar")
+set(CMAKE_CXX_COMPILER_RANLIB "${ARM_NONE_EABI_ROOT}arm-none-eabi-ranlib")
 
 # CMake Defaults
-set(CMAKE_AR "${ARM_NONE_EABI_ROOT}/arm-none-eabi-ar")
-set(CMAKE_LINKER "${ARM_NONE_EABI_ROOT}/arm-none-eabi-ld")
-set(CMAKE_NM "${ARM_NONE_EABI_ROOT}/arm-none-eabi-nm")
-set(CMAKE_OBJCOPY "${ARM_NONE_EABI_ROOT}/arm-none-eabi-objcopy")
-set(CMAKE_OBJDUMP "${ARM_NONE_EABI_ROOT}/arm-none-eabi-objdump")
-set(CMAKE_RANLIB "${ARM_NONE_EABI_ROOT}/arm-none-eabi-ranlib")
-set(CMAKE_STRIP "${ARM_NONE_EABI_ROOT}/arm-none-eabi-strip")
+set(CMAKE_AR "${ARM_NONE_EABI_ROOT}arm-none-eabi-ar")
+set(CMAKE_LINKER "${ARM_NONE_EABI_ROOT}arm-none-eabi-ld")
+set(CMAKE_NM "${ARM_NONE_EABI_ROOT}arm-none-eabi-nm")
+set(CMAKE_OBJCOPY "${ARM_NONE_EABI_ROOT}arm-none-eabi-objcopy")
+set(CMAKE_OBJDUMP "${ARM_NONE_EABI_ROOT}arm-none-eabi-objdump")
+set(CMAKE_RANLIB "${ARM_NONE_EABI_ROOT}arm-none-eabi-ranlib")
+set(CMAKE_STRIP "${ARM_NONE_EABI_ROOT}arm-none-eabi-strip")
 
 # Needed for the test program to compile when --configuring
 set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs" CACHE INTERNAL "")
